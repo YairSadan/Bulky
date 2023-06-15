4d938d6279b6548e9aa166b1abfbc8813608880f
יאיר סדן <yairsadan1@gmail.com>
2023-06-15 12:41:38 +0300

    Added the functionality to build initial database as you run the program

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -2,40 +2,37 @@ using Microsoft.EntityFrameworkCore;
 using Pet.DataAccess.Data;
 using Pet.DataAccess.Repository;
 using Pet.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace BulkyWeb {
-    public class Program {
-        public static void Main(string[] args) {
-            var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
 
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>(); 
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment()) {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
-    }
+await using (var scoped = app.Services.CreateAsyncScope()) {
+    using var context = scoped.ServiceProvider.GetService<ApplicationDbContext>();
+    await context.Database.MigrateAsync();
+    await context.Database.EnsureCreatedAsync();
 }
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
